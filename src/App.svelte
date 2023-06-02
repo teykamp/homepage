70f910f8dcd0f4d20dd5466470dac8e4e8ce83cd
eykamt <embergameslp@gmail.com>
2023-06-02 19:38:20 -0400

    move to tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
-  import "@picocss/pico/css/pico.min.css";
 </script>
 
 <main>
-  <div>
-    <Counter />
-  </div>
+  
 </main>
 
 <style>
