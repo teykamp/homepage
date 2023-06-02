16e3e2834e88c3dac8f3bb39ac5580be8985e61b
eykamt <embergameslp@gmail.com>
2023-06-02 14:47:04 -0400

    base template

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import "@picocss/pico/css/pico.min.css";
+</script>
+
+<main>
+  <div>
+    <Counter />
+  </div>
+</main>
+
+<style>
+  
+</style>
