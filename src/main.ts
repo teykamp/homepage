f988a1c0ce4ec1dea23c141c9a7ad2641835e636
eykamt <embergameslp@gmail.com>
2023-10-11 17:56:53 -0400

    add mdi

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 // import './style.css'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
