72a5177fcd9140577ed68ce1431405cbb9dbf307
eykamt <embergameslp@gmail.com>
2023-10-11 17:11:18 -0400

    add vuetify to app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './style.css'
+// import './style.css'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
-app.mount('#app')
+app.use(vuetify).mount('#app')
