809cacb9e5d39b1a8e252569e296214977ca0243
eykamt <embergameslp@gmail.com>
2023-10-27 22:50:56 -0400

    fix typo

--- src/stores/Store.ts
+++ src/stores/Store.ts
@@ -54,7 +54,7 @@ export const useLanguageStore = defineStore('language', {
               1: 'Successfully oversaw a comprehensive 6-month project from inception to completion, rapidly achieving milestones by boosting objective completion rates by 200% and 150% respectively.',
               2: 'Conceptualized a variety of chemical process diagrams and remediation procedures ranging from heavy metals to fluorinated hydrocarbons in a variety of chellenging environmental conditions.',
               3: 'Integral member of compliance unit tasked with the strategic sampling of both residential and industrial clientele, fostering collaborative interactions with residents and management.',
-              4: 'Effectively maintained and improved existing infrustructure and workflows at a variety of sites.',
+              4: 'Effectively maintained and improved existing infrastructure and workflows at a variety of sites.',
             }
           },
           2: {
