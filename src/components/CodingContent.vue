9a3ff4e32ac8d35ead5eb7642cc1f648cf033ad7
eykamt <embergameslp@gmail.com>
2023-11-16 22:53:01 -0500

    add image placeholders

--- src/components/CodingContent.vue
+++ src/components/CodingContent.vue
@@ -28,6 +28,14 @@
           style="height:130px"
           cover
         >
+        <template v-slot:placeholder>
+          <div class="d-flex align-center justify-center fill-height">
+            <v-progress-circular
+              color="grey-lighten-4"
+              indeterminate
+            ></v-progress-circular>
+          </div>
+        </template>
           <div class="card-overlay">
             <v-card-title class="">
               {{ project.title }}
